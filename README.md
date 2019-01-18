08e217f8060ea09aacee1f1564cc0baf183a68d7
Kmayer48 <kenny.mayer3@gmail.com>
2019-01-17 21:12:26 -0500

    first working push

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # Liri-Bot
 Node Command Line Bot
+
+## LIRI is similar to iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives you back data.
+
+## Prerequisites
+
+    * Terminal or Gitbash
+    * Node.js & NPM (Node Plugin Manager)
+    * Clone down repo
+    * Enter `npm install` in either Terminal or Gitbash
+
+## Available Commands
+#### On your command line enter one of the following commands:
+
+## Concert This
+
+    - **'node liri.js concert-this'** <band name goes here>
+
+### This will display results from the Bands in Town API. The response is set to return:
+
+    1. The lineup of the event
+    2. The venue name
+    3. The venute location
+    4. The date of the event converted with moment.js
+    
+## Spotify This Song
+
+    - **'node liri.js spotify-this-song'** <song name goes here>
+
+### This will display results from the Spotify API. The response is set to return:
+
+    1. Artist name(s)
+    2. Song name
+    3. Preview link
+        -note that not all songs have an available 30 second preview link if you receive null as a response
+    4. Album name
+
+###### If no response is entered the default search paramter is "The Sign" by Ace of Base
+
+## Movie This
+
+    - **'node liri.js movie-this'** <movie name goes here>
+
+### This will display results from the OMBD API. The response is set to return:
+
+    1. Movie title
+    2. Year of release
+    3. IMDB Rating
+    4. Rotten tomatoes rating
+    5. Country produced
+    6. Language
+    7. Plot
+    8. Movie Cast
+
+###### If no response is entered the default search paramter is "Mr. Nobody"
+
+## Do What It Says
+
+    - **'node liri.js do-what-it-says'**
+
+### This reads the random.txt file that is available in the repo. It's defualt search parameters are to "Spotify This" with the search paramter of "I want it that way". The text file can be alterted to one of the above commands before the first comma, followed by your search parameter. 
+
+## Authors
+
+    * Kenny Mayer
---
+++ liri.js
@@ -0,0 +1,180 @@
+require("dotenv").config();
+
+// API keys
+var keys = require("./keys.js");
+// axios package
+var axios = require("axios");
+// moment.js package
+var moment = require('moment');
+moment().format();
+// spotify package
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+// fs pacakge to handle read/write text file
+var fs = require("fs");
+
+// agrv[2] chooses user actions; agrv[3] is input parameter, i.e; song name
+var firstCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+// concatenate multiple words in 2nd user argument for search purposes
+for (var i = 4; i < process.argv.length; i++) {
+    secondCommand += ' ' + process.argv[i];
+}
+
+// switch statement to determine which funtion to run based on user input (second command)
+switch (firstCommand) {
+    case "concert-this":
+        concertThis(secondCommand);
+        break;
+    case "spotify-this-song":
+        spotifyThis(secondCommand);
+        break;
+    case "movie-this":
+        movieThis(secondCommand);
+        break;
+    case "do-what-it-says":
+        doIt();
+        break;
+    default:
+    console.log("Please input one of the recognized commands!");
+    break;
+};
+
+// bands in town API which returns the name of the venue, venue location, and date of the event
+function concertThis(secondCommand) {
+    axios.get("https://rest.bandsintown.com/artists/" + secondCommand + "/events?app_id=codingbootcamp")
+    .then(function(response) {    
+        for (var i = 0; i < response.data.length; i++) {
+            // saves datetime response into a variable converted using moment.js
+            var dateTime = moment(response.data[i].datetime).format("MM-DD-YYYY");
+            var concertResults = 
+                "\n--------------------Concert Results--------------------" +
+                // concert lineup
+                "\nLineup: " + response.data[i].lineup + 
+                // vanue name
+                "\nVenue Name: " + response.data[i].venue.name + 
+                // vanue city
+                "\nVenue Location: " + response.data[i].venue.city +
+                // vanue date of the event converted using moment.js
+                "\nDate of the Event: " + dateTime +
+                "\n-------------------------------------------------------\n";
+
+            console.log(concertResults);
+
+        // writes results to log.txt
+        fs.appendFile("log.txt", concertResults, function(err) {
+            // If an error was experienced it will log to the console
+            if (err) {
+                console.log(err);
+            }    
+        });
+        }
+        console.log("\n-------------------------------------------------------\n" +
+        "Your concert results were in logged a text file!" +
+        "\n-------------------------------------------------------\n");
+    })
+    // error response
+    .catch(function (error) {
+        console.log(error.response);
+    });
+    
+}
+
+// Spotify API call which returns the artist, song's name, preview link, and the album in which the song belongs to
+function spotifyThis(secondCommand) {
+    if(secondCommand === undefined) {
+        secondCommand = "The Sign";
+    }
+    spotify.search({ type: 'track', query: secondCommand })
+    .then(function(response) {
+        for (var i = 0; i < 10; i++) {
+            var spotifyResults = 
+                "\n--------------------Spotify Results--------------------" +
+                // artist
+                "\nArtist(s): " + response.tracks.items[i].artists[0].name + 
+                // song name
+                "\nSong Name: " + response.tracks.items[i].name +
+                // preview link
+                "\nPreview Link: " + response.tracks.items[i].preview_url +
+                // album name
+                "\nAlbum Name: " + response.tracks.items[i].album.name +
+                "\n-------------------------------------------------------\n";
+                    
+            console.log(spotifyResults);
+
+        // writes results to log.txt
+        fs.appendFile("log.txt", spotifyResults, function(err) {
+            // If an error was experienced it will log to the console
+            if (err) {
+                console.log(err);
+            }    
+            });
+        }
+        console.log("\n-------------------------------------------------------\n" +
+            "Your song results were in logged the text file!" +
+            "\n-------------------------------------------------------\n");
+    })
+    // error response
+    .catch(function(error) {
+        console.log(error.response);
+    });
+}
+
+function movieThis(secondCommand) {
+    if(secondCommand === undefined){
+        secondCommand = "mr nobody";
+        console.log("\n----------------------------------------" + 
+        "\nIf you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +
+        "\nIt's on Netflix!")        
+    } 
+    axios.get("https://www.omdbapi.com/?t=" + secondCommand + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+            var movieResults = 
+                "\n--------------------Movie Results----------------------" +
+                // movie title
+                "\nMovie Title: " + response.data.Title + 
+                // release year
+                "\nYear of Release: " + response.data.Year +
+                // IMDB rating
+                "\nIMDB Rating: " + response.data.imdbRating +
+                // rotten tomatoes rating
+                "\nRotten Tomatoes Rating: " + response.data.tomatoRating +
+                // country produced
+                "\nCountry Produced: " + response.data.Country +
+                // movie language
+                "\nLanguage: " + response.data.Language +
+                // movie plot
+                "\nPlot: " + response.data.Plot +
+                // movie cast
+                "\nMovie Cast: " + response.data.Actors +
+                "\n-------------------------------------------------------\n";
+
+            console.log(movieResults);
+
+        // writes results to log.txt
+        fs.appendFile("log.txt", movieResults, function(err) {
+            // If an error was experienced it will log to the console
+            if (err) {
+                console.log(err);
+            }    
+            });
+        console.log("\n-------------------------------------------------------\n" +
+                "Your movie results were in logged the text file!" +
+                "\n-------------------------------------------------------\n");
+    })
+    // error response
+    .catch(function (error) {
+        console.log(error.response);
+    });
+}
+
+function doIt(){
+    // reads random.txt file
+    fs.readFile('random.txt', "utf8", function(error, data){
+    // splits text in file by a comma delimter 
+      var txt = data.split(',');
+    // returns value from text[1] and performs function
+      spotifyThis(txt[1]);
+    });
+}
